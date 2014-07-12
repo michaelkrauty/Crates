e93884001190eb29f7ff4db748a641a79d9558a6
michaelkrauty <michaelkrauty@gmail.com>
2014-07-12 15:07:07 -0700

    first commit

---
+++ src/main/java/me/michaelkrauty/Crates/Main.java
@@ -0,0 +1,17 @@
+package me.michaelkrauty.Crates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created on 7/12/2014.
+ *
+ * @author michaelkrauty
+ */
+public class Main extends JavaPlugin {
+
+	public static Main main;
+
+	public void onEnable() {
+		main = this;
+	}
+}
